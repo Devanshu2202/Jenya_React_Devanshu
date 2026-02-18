05db488d5099f0255257881bf59e1cee21f0e3c7
Devanshu Mishra <devmishra2202@gmail.com>
2026-02-18 15:58:34 +0530

    call login API and store token and refresh token in localstorage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,37 @@
+import { useState } from "react";
+
 const LoginPage = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch('https://dummyjson.com/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    username,
+                    password
+                })
+            });
+            const data = await response.json();
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('refreshToken', data.refreshToken);
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <>
             <h1 className="text-3xl font-bold underline">Login Page</h1>
-            <form className="flex flex-col gap-4">
-                <input type="text" placeholder="Username" className="border border-gray-300 rounded-md p-2" />
-                <input type="password" placeholder="Password" className="border border-gray-300 rounded-md p-2" />
+            <form className="flex flex-col gap-4" onSubmit={handleLogin}>
+                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" className="border border-gray-300 rounded-md p-2" />
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" className="border border-gray-300 rounded-md p-2" />
                 <button type="submit" className="bg-blue-500 text-white rounded-md p-2">Login</button>
             </form>
         </>
